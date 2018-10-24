190019d4f041301ac22125a645b295e2304506c0
maartenplieger <maartenplieger@gmail.com>
2018-10-24 12:23:26 +0000

    Initial commit of adaguc-webmapjs

---
+++ WMJSCanvasBuffer.js
@@ -0,0 +1,302 @@
+import WMJSImage from './WMJSImage.js';
+import { error, debug } from './WMJSConstants.js';
+import { isDefined } from './WMJSTools.js';
+import { $ } from './WMJSExternalDependencies.js';
+export default class WMJSCanvasBuffer {
+  constructor (webmapJSCallback, _type, _imageStore, w, h) {
+    this.canvas = $('<canvas/>', { 'class':'WMJSCanvasBuffer' }).width(w).height(h);
+    this._ctx = this.canvas[0].getContext('2d');
+    this._ctx.canvas.width = w;
+    this._ctx.canvas.height = h;
+    // TODO: Check if this really gives sharper instead of blurier images...
+    // this._ctx.webkitImageSmoothingEnabled = true;
+    // this._ctx.imageSmoothingQuality igh';
+    // this._ctx.msImageSmoothingEnabled = true;
+    // this._ctx.imageSmoothingEnabled = tr
+    this._imageStore = _imageStore;
+    this.ready = true;
+    this.hidden = true;
+    this.layerstodisplay = [];
+    this.layers = [];
+    this._defaultImage = new WMJSImage('webmapjs/img/stoploading.png', () => {
+      console.log('no image loaded');
+      this._statDivBufferImageLoaded();
+    }, this._type);
+    this._currentbbox = undefined;
+    this._currentnewbbox = undefined;
+
+    this._width = w;
+    this._height = h;
+    this._type = _type;
+    this._webmapJSCallback = webmapJSCallback;
+    if (this._type === 'imagebuffer') {
+      this.canvas.addClass('wmjsimagebuffer');
+    }
+    if (this._type === 'legendbuffer') {
+      this.canvas.addClass('wmjslegendbuffer');
+    }
+
+    this.canvas.addClass('WMJSCanvasBuffer-noselect');
+    /* Bind */
+    this.getCanvasContext = this.getCanvasContext.bind(this);
+    this.imageLoadComplete = this.imageLoadComplete.bind(this);
+    this._statDivBufferImageLoaded = this._statDivBufferImageLoaded.bind(this);
+    this.hide = this.hide.bind(this);
+    this.display = this.display.bind(this);
+    this.finishedLoading = this.finishedLoading.bind(this);
+    this.setPosition = this.setPosition.bind(this);
+    this.setSize = this.setSize.bind(this);
+    this.resize = this.resize.bind(this);
+    this.load = this.load.bind(this);
+    this.setSrc = this.setSrc.bind(this);
+    this._getPixelCoordFromGeoCoord = this._getPixelCoordFromGeoCoord.bind(this);
+    this.setBBOX = this.setBBOX.bind(this);
+    this.setOpacity = this.setOpacity.bind(this);
+    this.getBuffer = this.getBuffer.bind(this);
+  }
+
+  getCanvasContext () {
+    return this._ctx;
+  };
+
+  imageLoadComplete (image) {
+    this._statDivBufferImageLoaded();
+    this._webmapJSCallback.triggerEvent('onimageload');
+    if (this._type === 'imagebuffer') {
+      this._webmapJSCallback.triggerEvent('onimagebufferimageload', image);
+    }
+  };
+
+  _statDivBufferImageLoaded () {
+    for (let j = 0; j < this.layers.length; j++) {
+      if (this.layers[j].isLoaded() === false) {
+        return;
+      }
+    }
+    this.finishedLoading();
+  };
+
+  hide () {
+    this.hidden = true;
+    this.canvas.hide();
+    this.layers = [];
+  };
+
+  display (newbbox, loadedbbox) {
+    let errorList = [];
+    if ((newbbox && !loadedbbox)) {
+      console.error('skipping WMJSCanvasBuffer:display because newbbox is undefined');
+      return;
+    }
+
+    this.hidden = false;
+    this._ctx.globalAlpha = 1;
+
+    if (this._type === 'legendbuffer') {
+      this._ctx.clearRect(0, 0, this._width, this._height);
+    }
+    if (this._type === 'imagebuffer') {
+      this._ctx.beginPath();
+      this._ctx.rect(0, 0, this._width, this._height);
+      this._ctx.fillStyle = 'white';
+      this._ctx.fill();
+      this._webmapJSCallback.triggerEvent('beforecanvasstartdraw', this._ctx);
+    }
+
+    /* Calculcate new pos */
+    let coord1, coord2;
+    if (newbbox) {
+      let b1 = loadedbbox;// this.bbox;
+      // if(newbbox == undefined)newbbox=b1;
+      let b2 = newbbox;
+      coord1 = this._getPixelCoordFromGeoCoord({ x:b1.left, y:b1.top }, b2);
+      coord2 = this._getPixelCoordFromGeoCoord({ x:b1.right, y:b1.bottom }, b2);
+    }
+
+    let legendPosX = 0;
+    for (let j = 0; j < this.layerstodisplay.length; j++) {
+      this.layerstodisplay[j].setSize(this._width, this._height);
+      if (this.layerstodisplay[j].hasError() === false) {
+        // Draw
+        let op = this.layerstodisplay[j].getOpacity();
+        this._ctx.globalAlpha = op;
+        let el = this.layerstodisplay[j].getElement()[0];
+        if (this._type === 'legendbuffer') {
+          let legendW = parseInt(el.width) + 4;
+          let legendH = parseInt(el.height) + 4;
+          legendPosX += (legendW + 4);
+          let legendX = this._width - legendPosX + 2;
+          let legendY = this._height - (legendH) - 2;
+          this._ctx.beginPath();
+          this._ctx.fillStyle = '#FFFFFF';
+          this._ctx.lineWidth = 0.3;
+          this._ctx.globalAlpha = 0.5;
+          this._ctx.strokeStyle = '#000000';
+
+          this._ctx.rect(parseInt(legendX) + 0.5, parseInt(legendY) + 0.5, legendW, legendH);
+          this._ctx.fill();
+          this._ctx.stroke();
+          this._ctx.globalAlpha = 1.0;
+          this._ctx.drawImage(el, legendX, legendY);
+        } else {
+          if (newbbox) {
+            let imageX = parseInt(coord1.x + 0.5);
+            let imageY = parseInt(coord1.y + 0.5);
+            let imageW = parseInt((coord2.x - coord1.x) + 0.5);
+            let imageH = parseInt((coord2.y - coord1.y) + 0.5);
+
+            if ((imageW) === parseInt(this._ctx.canvas.width) && (imageH) === parseInt(this._ctx.canvas.height)) {
+              this._ctx.drawImage(el, imageX, imageY);
+            } else {
+              this._ctx.drawImage(el, imageX, imageY, imageW, imageH);
+            }
+          } else {
+            this._ctx.drawImage(el, 0, 0, this._width, this._height);
+          }
+        }
+      } else {
+        errorList.push(this.layerstodisplay[j]);
+      }
+    }
+    this._ctx.globalAlpha = 1;
+
+    /* Display errors */
+    if (this._type === 'imagebuffer') {
+      if (errorList.length > 0) {
+        this._webmapJSCallback.triggerEvent('canvasonerror', errorList);
+      }
+    }
+    if (this._type === 'imagebuffer') {
+      this._webmapJSCallback.triggerEvent('beforecanvasdisplay', this._ctx);
+    }
+
+    this.canvas.show();
+    if (this._type === 'imagebuffer') {
+      this._webmapJSCallback.triggerEvent('aftercanvasdisplay', this._ctx);
+    }
+  };
+
+  finishedLoading () {
+    if (this.ready) return;
+    this.ready = true;
+    for (let j = 0; j < this.layers.length; j++) {
+      this.layerstodisplay[j] = this.layers[j];
+      if (this.layers[j].hasError()) {
+        error('Unable to get image <a target="_blank" href="' +
+          this.layerstodisplay[j].getSrc() + '">' + this.layerstodisplay[j].getSrc() + '</a>', false);
+      }
+    }
+    try {
+      if (isDefined(this.onLoadReadyFunction)) {
+        this.onLoadReadyFunction(this);
+      }
+    } catch (e) {
+      error('Exception in Divbuffer::finishedLoading: ' + e);
+    }
+  };
+
+  setPosition (x, y) {
+    console.log('setPosition is deprecated');
+  };
+
+  setSize (w, h) {
+    console.log('setSize is deprecated');
+  };
+
+  resize (w, h) {
+    w = parseInt(w);
+    h = parseInt(h);
+    if (this._width === w && this._height === h) return;
+    this._width = w;
+    this._height = h;
+    this.canvas.width(w);
+    this.canvas.height(h);
+    this._ctx.canvas.height = h;
+    this._ctx.canvas.width = w;
+  };
+
+  load (callback) {
+    if (this.ready === false) {
+      // console.log(" ===== Still busy ====== ");
+      return;
+    }
+    this.ready = false;
+    this.layerstodisplay = [];
+
+    // console.log("WMJSCanvasBuffer:load");
+    // this.setPosition(0,0);
+    if (callback) { this.onLoadReadyFunction = callback; } else this.onLoadReadyFunction = {};
+    this.nrLoading = 0;
+    // console.log("WMJSCanvasBuffer:this.layers.length = "+this.layers.length);
+
+    for (let j = 0; j < this.layers.length; j++) {
+      this.layers[j].loadThisOne = false;
+
+      if (this.layers[j].isLoaded() === false) {
+        this.layers[j].loadThisOne = true;
+        this.nrLoading++;
+      }
+    }
+    // console.log("WMJSCanvasBuffer.nrLoading = "+this.nrLoading +" nrImages = "+this.layers.length );
+    if (this.nrLoading === 0) {
+      this._statDivBufferImageLoaded();
+    } else {
+      if (this._type === 'imagebuffer') { debug('GetMap:'); }
+      if (this._type === 'legendbuffer') { debug('GetLegendGraphic:'); }
+      for (let j = 0; j < this.layers.length; j++) {
+        if (this.layers[j].loadThisOne === true) {
+          debug("<a target='_blank' href='" + this.layers[j].getSrc() + "'>" + this.layers[j].getSrc() + '</a>', false);
+        }
+      }
+
+      for (let j = 0; j < this.layers.length; j++) {
+        if (this.layers[j].loadThisOne === true) {
+          // console.log('WMJSCanvasBuffer.loading = ' + this.layers[j].getSrc());
+          this.layers[j].load();
+        }
+      }
+    }
+  };
+
+  setSrc (layerIndex, imageSource, width, height, linkedInfo) {
+    if (!isDefined(imageSource)) { console.log('undefined'); return; }
+    while (layerIndex >= this.layers.length) {
+      this.layers.push(this._defaultImage);
+    }
+    let image = this._imageStore.getImage(imageSource);
+    image.setZIndex(layerIndex);
+    image.linkedInfo = linkedInfo;
+    this.layers[layerIndex] = image;
+  };
+
+  _getPixelCoordFromGeoCoord (coordinates, b) {
+    let x = (this._width * (coordinates.x - b.left)) / (b.right - b.left);
+    let y = (this._height * (coordinates.y - b.top)) / (b.bottom - b.top);
+    return { x:x, y:y };
+  };
+
+  setBBOX (newbbox, loadedbbox) {
+    if (this._currentbbox === loadedbbox + '' && this._currentnewbbox === newbbox + '') return;
+    this._currentbbox = loadedbbox + '';
+    this._currentnewbbox = newbbox + '';
+    if (this.hidden === false) {
+      this.display(newbbox, loadedbbox);
+    }
+  };
+
+  setOpacity (layerIndex, opacity) {
+    if (layerIndex >= this.layers.length) {
+      error('setOpacity :invalid id');
+      return;
+    }
+    let op = parseFloat(opacity);
+
+    if (this.layers[layerIndex].getOpacity() !== op) {
+      this.layers[layerIndex].setOpacity(op);
+    }
+  };
+
+  getBuffer () {
+    return this.canvas;
+  };
+};
---
+++ WMJSListener.js
@@ -0,0 +1,103 @@
+/*
+ * Name        : WMJSListener
+ * Description : Provides basic functionality for a callback mechanism
+ * Author      : MaartenPlieger (plieger at knmi.nl)
+ * Copyright KNMI
+ */
+class CallBackFunction {
+  constructor () {
+    this.name = undefined;
+    this.functionpointer = undefined;
+    this.finished = 0;
+    this.keepOnCall = false;
+  }
+};
+
+export default class WMJSListener {
+  constructor () {
+    this._callBacks = [];
+    this._numCallBacks = 0;
+    this._suspendedEvents = [];
+    this.addToCallback = this.addToCallback.bind(this);
+    this.removeEvents = this.removeEvents.bind(this);
+    this.suspendEvent = this.suspendEvent.bind(this);
+    this.resumeEvent = this.resumeEvent.bind(this);
+    this.triggerEvent = this.triggerEvent.bind(this);
+  }
+  /* Add multiple functions which will be called after the event with the same name is triggered */
+  addToCallback (name, functionpointer, keepOnCall) {
+    let cbp = -1; /* callbackpointer */
+    if (!keepOnCall) {
+      keepOnCall = false;
+    }
+    for (let j = 0; j < this._numCallBacks; j++) {
+      // A callback list index pointer. if finished==1, then this index may be replaced by a new one.
+      if (this._callBacks[j].finished === 1) { cbp = j; break; }
+      // If the current callback already exist, we will simply keep it
+      if (this._callBacks[j].name === name && this._callBacks[j].functionpointer === functionpointer) {
+        // this._callBacks[j].timesAdded++;
+        // console.log('listener already added: ', name);
+        this._callBacks[j].keepOnCall = keepOnCall;
+        return false;
+      }
+    }
+    if (cbp === -1) {
+      cbp = this._numCallBacks;
+      this._numCallBacks++;
+      this._callBacks[cbp] = new CallBackFunction();
+    } else {
+      // console.log('replacing old unused listener: ', name);
+    }
+    this._callBacks[cbp].name = name;
+    this._callBacks[cbp].functionpointer = functionpointer;
+    this._callBacks[cbp].finished = 0;
+    this._callBacks[cbp].keepOnCall = keepOnCall;
+    // this._callBacks[j].timesAdded = 0;
+    return true;
+  };
+
+  removeEvents (name, f) {
+    for (let j = 0; j < this._numCallBacks; j++) {
+      if (this._callBacks[j].finished === 0) {
+        if (this._callBacks[j].name === name) {
+          if (!f) {
+            this._callBacks[j].finished = 1;
+          } else if (this._callBacks[j].functionpointer === f) {
+            this._callBacks[j].finished = 1;
+          }
+        }
+      }
+    }
+  };
+
+  suspendEvent (name) {
+    this._suspendedEvents[name] = true;
+  };
+
+  resumeEvent (name) {
+    this._suspendedEvents[name] = false;
+  };
+
+  // Trigger an event with a name
+  triggerEvent (name, param) {
+    if (this._suspendedEvents[name] === true) {
+      return;
+    }
+    let returnList = [];
+    for (let j = 0; j < this._numCallBacks; j++) {
+      if (this._callBacks[j].finished === 0) {
+        if (this._callBacks[j].name === name) {
+          if (this._callBacks[j].keepOnCall === false) {
+            this._callBacks[j].finished = 1;
+          }
+          try {
+            returnList.push(this._callBacks[j].functionpointer(param, this));
+          } catch (e) {
+            console.log('Error for event [' + name + ']: ', param, e);
+          }
+        }
+      }
+    }
+    return returnList;
+  };
+};
