2080ad9c950a25ab9558ba267eed31a9279ba9be
maartenplieger <maartenplieger@gmail.com>
2018-10-29 08:49:06 +0000

    Webpack and babel creating main and module via package.json

--- src/index.js
+++ src/index.js
@@ -8,39 +8,6 @@ import { parseISO8601DateToDate } from  './WMJSTime.js';
 import WMJSBBOX from './WMJSBBOX.js';
 import I18n from  './I18n/lang.en.js';
 
-// exports = {
-//   WMJSMap: WMJSMap,
-//   isDefined: isDefined,
-//   WMJSLayer: WMJSLayer,
-//   getUrlVars: getUrlVars,
-//   checkIfHashTagChanged: checkIfHashTagChanged,
-//   WMJSTimer: WMJSTimer,
-//   WMJSGetServiceFromStore: WMJSGetServiceFromStore,
-//   WMJScheckURL: WMJScheckURL,
-//   URLEncode: URLEncode,
-//   URLDecode: URLDecode,
-//   WMJSDateOutSideRange: WMJSDateOutSideRange,
-//   WMJSDateTooEarlyString: WMJSDateTooEarlyString,
-//   WMJSDateTooLateString: WMJSDateTooLateString,
-//   WMJSEmptyLayerName: WMJSEmptyLayerName,
-//   WMJSEmptyLayerTitle: WMJSEmptyLayerTitle,
-//   parseISO8601DateToDate: parseISO8601DateToDate,
-//         I18n: I18n,
-//         addMouseWheelEvent: addMouseWheelEvent,
-//         removeMouseWheelEvent: removeMouseWheelEvent,
-//         WMJSKVP: WMJSKVP,
-//         composeUrlObjectFromURL: composeUrlObjectFromURL,
-//         WMJSBBOX: WMJSBBOX,
-//         toArray: toArray
-// }
-// 
-// console.log(exports);
-
-var exports = {};
-exports.printMsg = function() {
-  console.log("This is a message from the adaguc-webmapjs package");
-}
-
 export { WMJSMap, 
   isDefined, 
   WMJSLayer, 
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,38 @@
 const path = require('path');
+require("babel-register");
 
 module.exports = {
   entry: './src/index.js',
+  mode:'production',
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'adaguc-webmapjs.js'
-  }
+    filename: 'adaguc-webmapjs.js',
+    library: "adaguc-webmapjs",
+    libraryTarget: "umd" 
+  },
+  externals:{
+    jquery: 'jQuery',
+    moment: 'moment',
+    proj4:  'proj4'
+  },
+  module: {
+    rules : [
+      // JavaScript/JSX Files
+      {
+      test: /\.js$/,
+      exclude: /node_modules/,
+      use: ['babel-loader']
+      },
+      // CSS Files
+      {
+      test: /\.css$/,
+      use: ['style-loader', 'css-loader']
+      }
+    ]
+  },
+  plugins: []
 };
+module.loaders = [
+    { test: /\.js$/, exclude: /node_modules/, use: "babel-loader" }
+  ]
+
