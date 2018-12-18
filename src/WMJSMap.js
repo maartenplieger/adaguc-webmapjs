86fcf7a33f48735d4ace6ed6eded6569eb73230e
maartenplieger <maartenplieger@gmail.com>
2018-12-18 12:48:16 +0100

    v3.2.7

--- src/WMJSMap.js
+++ src/WMJSMap.js
@@ -84,7 +84,7 @@ class GetFeatureInfoObject {
   */
 export default class WMJSMap {
   constructor (_element, _xml2jsonrequestURL) {
-    this.WebMapJSMapVersion = '3.2.6';
+    this.WebMapJSMapVersion = '3.2.7';
     this.base = './';
     this.noimage = undefined;
     this.showDialog = true;
