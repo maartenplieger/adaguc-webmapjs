d7d16a5d812f4a3ec8c9c33faa2ea061abe836a5
Maarten Plieger <maartenplieger@gmail.com>
2019-07-05 16:00:10 +0200

    Fixed linting warnings and added baselayer attribution

--- src/WMJSExternalDependencies.js
+++ src/WMJSExternalDependencies.js
@@ -1,7 +1,7 @@
 /* Moment, proj4 and jquery are externals, e.g. not packaged in adaguc-webmapjs: */
 import moment from 'moment';
 import proj4 from 'proj4';
-import $ from 'jquery';
 
 /* Jquery should be globally availble */
-export { $, moment, proj4 };
+var jquery = window.jQuery || window.$ || global.$ || global.jQuery;
+export { jquery, moment, proj4 };
